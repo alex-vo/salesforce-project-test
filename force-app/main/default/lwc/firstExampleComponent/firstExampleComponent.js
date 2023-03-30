3429a6aeade1fb5ec7f51cf329c85bb75f96e35b
Aleksei <avoroncovs@gmail.com>
2023-03-30 19:28:55 +0300

    sample ajax request

--- force-app/main/default/lwc/firstExampleComponent/firstExampleComponent.js
+++ force-app/main/default/lwc/firstExampleComponent/firstExampleComponent.js
@@ -1,3 +1,18 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
+import getValue from '@salesforce/apex/firstApex.getValue';
 
-export default class FirstExampleComponent extends LightningElement {}
+export default class FirstExampleComponent extends LightningElement {
+
+  @api recordId;
+
+  responzz;
+
+  hello(){
+    console.log('recordId ' + this.recordId);
+
+    getValue({recordId: `${this.recordId}`})
+      .then(response => {
+        this.responzz = response;
+      });
+  }
+}
